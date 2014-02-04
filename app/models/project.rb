60fd5262d22fdd367adf97d7ca02092a854bcc07
Jens Ulferts <j.ulferts@finn.de>
2014-02-04 19:10:13 +0100

    uses allowed scope for project member filtering

--- app/models/project.rb
+++ app/models/project.rb
@@ -47,14 +47,6 @@ class Project < ActiveRecord::Base
   # Specific overidden Activities
   has_many :time_entry_activities
   has_many :members, :include => [:user, :roles], :conditions => "#{User.table_name}.type='User' AND #{User.table_name}.status=#{User::STATUSES[:active]}"
-  has_many :possible_assignee_members,
-           :class_name => 'Member',
-           :include => [:principal, :roles],
-           :conditions => Proc.new { self.class.possible_assignees_condition }
-  has_many :possible_responsible_members,
-           :class_name => 'Member',
-           :include => [:principal, :roles],
-           :conditions => Proc.new { self.class.possible_responsibles_condition }
   has_many :memberships, :class_name => 'Member'
   has_many :member_principals, :class_name => 'Member',
                                :include => :principal,
@@ -554,12 +546,16 @@ class Project < ActiveRecord::Base
 
   # Users/groups a work_package can be assigned to
   def possible_assignees
-    possible_assignee_members.map(&:principal).compact.sort
+    accepted_types = Setting.work_package_group_assignment? ?
+                            [Group.to_s, User.to_s] :
+                            [User.to_s]
+
+    possible_principals(accepted_types)
   end
 
   # Users who can become responsible for a work_package
   def possible_responsibles
-    possible_responsible_members.map(&:principal).compact.sort
+    possible_principals([User.to_s])
   end
 
   # Returns the mail adresses of users that should be always notified on project events
@@ -896,5 +892,14 @@ class Project < ActiveRecord::Base
       'User', User::STATUSES[:active], true]
 
     sanitize_sql_array condition
+  end
+
+  def possible_principals(accepted_types)
+    members_with_assignable_roles.where(users: { type: accepted_types })
+  end
+
+  def members_with_assignable_roles
+    Allowance.principals(project: self)
+             .where(roles: { assignable: true }).order_by_name
   end
 end
