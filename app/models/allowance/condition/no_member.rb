6e63dce669c4a5392426a01d3224341c87930b46
Jens Ulferts <j.ulferts@finn.de>
2014-03-10 23:01:57 +0100

    adds spec for no_member condition

--- app/models/allowance/condition/no_member.rb
+++ app/models/allowance/condition/no_member.rb
@@ -2,10 +2,8 @@ module Allowance::Condition
   class NoMember < Base
     table Member
 
-    def arel_statement(project: nil, **ignored)
-      #if project.nil? || project.is_public?
-        members[:id].eq(nil)
-      #end
+    def arel_statement(**ignored)
+      members[:id].eq(nil)
     end
   end
 end
