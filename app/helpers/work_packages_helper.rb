667fae72a558dee31924eafd8d39055f98b90dfa
Jens Ulferts <j.ulferts@finn.de>
2014-03-11 08:51:29 +0100

    removes duplicate user load optimization

--- app/helpers/work_packages_helper.rb
+++ app/helpers/work_packages_helper.rb
@@ -511,18 +511,14 @@ module WorkPackagesHelper
   end
 
   def work_package_form_assignee_attribute(form, work_package, locals = {})
-    selected_fields = (['users.id', 'users.type'] + User::USER_FORMATS_STRUCTURE.values.flatten.uniq.map(&:to_s)).join(', ')
-
-    options = work_package.assignable_assignees.select(selected_fields).map {|m| [m.name, m.id]}
+    options = work_package.assignable_assignees.select_only_name_attributes.map {|m| [m.name, m.id]}
 
     WorkPackageAttribute.new(:assignee,
                              form.select(:assigned_to_id, options, :include_blank => true))
   end
 
   def work_package_form_responsible_attribute(form, work_package, locals = {})
-    selected_fields = (['users.id', 'users.type'] + User::USER_FORMATS_STRUCTURE.values.flatten.uniq.map(&:to_s)).join(', ')
-
-    options = work_package.assignable_responsibles.select(selected_fields).map {|m| [m.name, m.id]}
+    options = work_package.assignable_responsibles.select_only_name_attributes.map {|m| [m.name, m.id]}
 
     WorkPackageAttribute.new(:responsible,
                              form.select(:responsible_id, options, :include_blank => true))
--- lib/plugins/acts_as_watchable/lib/acts_as_watchable.rb
+++ lib/plugins/acts_as_watchable/lib/acts_as_watchable.rb
@@ -91,11 +91,10 @@ module Redmine
         def possible_watcher_users
           permission = self.class.acts_as_watchable_options[:permission]
 
-          # TODO: somehow get this standard #distinct method to work
-
-          selected_fields = (['users.id', 'users.type'] + User::USER_FORMATS_STRUCTURE.values.flatten.uniq.map(&:to_s)).join(', ')
-
-          User.allowed(permission, self.project, admin_pass: false).not_builtin.order_by_name.select(selected_fields)
+          User.allowed(permission, self.project, admin_pass: false)
+              .not_builtin
+              .order_by_name
+              .select_only_name_attributes
         end
 
         # Returns an array of users that are proposed as watchers
